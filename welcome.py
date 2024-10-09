5380a93c9f28b8e4cfc992e696229fbede9f150a
avnighai2 <avnighai2@gmail.com>
2024-10-09 19:44:15 -0400

    lesson 2

---
+++ welcome.py
@@ -0,0 +1,10 @@
+print( "hello world")
+name = "Avni"
+age = 15
+print( " my name is " , name + " and my age is ",age )
+print(f"my name is {name} and my age is {age}")
+gender = input(" please enter your gender : ")
+print(f"my gender is {gender}")
+score = input(" please enter your score ")
+print(f" my score is {score}")
+print(" welcome to coding. \n this is my second class")
