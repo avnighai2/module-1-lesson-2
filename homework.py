9049267da9b9a7b7372398184970b3f23d32f306
avnighai2 <avnighai2@gmail.com>
2024-10-09 20:03:15 -0400

    homework

---
+++ homework.py
@@ -0,0 +1,17 @@
+
+name1 = input( " Enter your name:")
+birthday1 = input(" Enter your birthday ") #(yyyy/mm/dd)
+
+name2 = input( " Enter your name:")
+birthday2 = input(" Enter your birthday ") 
+
+name3 = input( " Enter your name:")
+birthday3 = input(" Enter your birthday ") 
+
+name4 = input( " Enter your name:")
+birthday4 = input(" Enter your birthday ") 
+
+name5 = input( " Enter your name:")
+birthday5 = input(" Enter your birthday ") 
+
+print(f"{name1}: {birthday1}, {name2}: {birthday2}, {name3}: {birthday3}, {name4}: {birthday4}, {name5}: {birthday5}")
